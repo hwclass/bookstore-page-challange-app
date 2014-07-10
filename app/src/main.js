70e3465594abe4621b82b1148002bd00077af804
hwclass <harderworking.class@gmail.com>
2014-07-10 16:28:23 +0300

    Removing the config object from main.js file.

--- app/src/main.js
+++ app/src/main.js
@@ -1,30 +1,4 @@
-var Vue = require('vue')
-
-Vue.config({
-
-    // prefix for directives
-    prefix: 'app',
-
-    // log debug info
-    debug: false,
-
-    // suppress warnings
-    silent: false,
-
-    // CSS class attached for entering transition
-    enterClass: 'v-enter',
-
-    // CSS class attached for leaving transition
-    leaveClass: 'v-leave',
-
-    // Interpolate mustache bindings
-    interpolate: true,
-
-    // Interpolation delimiters
-    // default value translates to {{ }} and {{{ }}}
-    delimiters: ['{', '}']
-
-});
+var Vue = require('vue');
 
 new Vue({
 
