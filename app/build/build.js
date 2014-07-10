4d62fd2f841ceb9b179ce0e6b2e79a63827c7cb8
hwclass <harderworking.class@gmail.com>
2014-07-10 16:57:59 +0300

    Config and data components initialized.

--- app/build/build.js
+++ app/build/build.js
@@ -4698,6 +4698,47 @@ module.exports = {
 
 }
 });
+require.register("config/src/config.js", function(exports, require, module){
+module.exports = {
+
+	prefix: 'app',
+
+	debug: false,
+
+	silent: false,
+
+	enterClass: 'v-enter',
+
+	leaveClass: 'v-leave',
+
+	interpolate: true,
+
+	delimiters: ['{', '}']
+
+};
+});
+require.register("data/src/book.js", function(exports, require, module){
+module.exports = {
+    "title": "Gold",
+    "description": "The spectacular new novel by the bestselling and critically acclaimed author of the reader and bookseller favourite, Little Bee. In the tradition of his beloved previous novel, Little Bee, Chris Cleave again gives us an elegant, funny, passionate story about friendship, marriage, parenthood, tragedy, and redemption. This time, the setting is the upcoming London Olympics. Gold is the story of two women, Zoe and Kate, world-class cyclists who have been friends and rivals since their first day of elite training years ago. They have loved, fought, betrayed, forgiven, lost, consoled, triumphed, and grown up together. Now, on the eve of London 2012, their last Olympics, the two must compete for the one remaining spot on their team. In doing so, the women will be tested to their physical, mental and emotional limits. They will confront each other and their own mortality, and be asked to decide: What will you sacrifice for the people you love?",
+    "image_url": "img/cover.jpg",
+    "author": "Chris Cleave",
+    "isbn": "9780385677165",
+    "format": "ebook",
+    "pages": "368",
+    "publication_date": "06/05/2012",
+    "quotes": [
+        {"quote" : "A true artist is not one who is inspired, but one who inspires others."},
+        {"quote" : "You tell me, and I forget. You teach me, and I remember. You involve me, and I learn."},
+        {"quote" : "A great thought begins by seeing something differently, with a shift of the mind's eye."}
+    ],
+    "products": [
+        {"type" : "eBook", "price" : "12.99"},
+        {"type" : "CD-AUDIO", "price" : "30.13"},
+        {"type" : "HARDCOVER", "price" : "24.32"}
+    ]
+}
+});
 require.register("navbar/index.js", function(exports, require, module){
 module.exports = {
   className: 'navbar navbar-inverse navbar-fixed-top',
@@ -4717,60 +4758,39 @@ module.exports = {
 }
 });
 require.register("vue-component-example/src/config.js", function(exports, require, module){
-Vue.config({
+module.exports = {
 
-	// prefix for directives
+	/* prefix for directives */
 	prefix: 'app',
 
-	// log debug info
+	/* log debug info */
 	debug: false,
 
-	// suppress warnings
+	/* suppress warnings */
 	silent: false,
 
-	// CSS class attached for entering transition
+	/* CSS class attached for entering transitionc*/
 	enterClass: 'v-enter',
 
-	// CSS class attached for leaving transition
+	/* CSS class attached for leaving transitionc*/
 	leaveClass: 'v-leave',
 
-	// Interpolate mustache bindings
+	/* Interpolate mustache bindings */
 	interpolate: true,
 
-	// Interpolation delimiters
-	// default value translates to {{ }} and {{{ }}}
+	/* Interpolation delimiters
+	 default value translates to {{ }} and {{{ }}}*/
 	delimiters: ['{', '}']
 
-});
+};
 });
 require.register("vue-component-example/src/main.js", function(exports, require, module){
+/*Vue framework imported*/
 var Vue = require('vue')
 
-Vue.config({
-
-    // prefix for directives
-    prefix: 'app',
-
-    // log debug info
-    debug: false,
-
-    // suppress warnings
-    silent: false,
-
-    // CSS class attached for entering transition
-    enterClass: 'v-enter',
-
-    // CSS class attached for leaving transition
-    leaveClass: 'v-leave',
-
-    // Interpolate mustache bindings
-    interpolate: true,
-
-    // Interpolation delimiters
-    // default value translates to {{ }} and {{{ }}}
-    delimiters: ['{', '}']
-
-});
+/*config component imported*/
+var config = require('config');
+Vue.config(config);
 
 new Vue({
 
@@ -4804,26 +4824,7 @@ new Vue({
         	p : 'Use this document as a way to quickly start any new project. All you get is this text and a mostly barebones HTML document.'
         },
         currentView : 'main',
-        book : {
-            "title": "Gold",
-            "description": "The spectacular new novel by the bestselling and critically acclaimed author of the reader and bookseller favourite, Little Bee. In the tradition of his beloved previous novel, Little Bee, Chris Cleave again gives us an elegant, funny, passionate story about friendship, marriage, parenthood, tragedy, and redemption. This time, the setting is the upcoming London Olympics. Gold is the story of two women, Zoe and Kate, world-class cyclists who have been friends and rivals since their first day of elite training years ago. They have loved, fought, betrayed, forgiven, lost, consoled, triumphed, and grown up together. Now, on the eve of London 2012, their last Olympics, the two must compete for the one remaining spot on their team. In doing so, the women will be tested to their physical, mental and emotional limits. They will confront each other and their own mortality, and be asked to decide: What will you sacrifice for the people you love?",
-            "image_url": "img/cover.jpg",
-            "author": "Chris Cleave",
-            "isbn": "9780385677165",
-            "format": "ebook",
-            "pages": "368",
-            "publication_date": "06/05/2012",
-            "quotes": [
-                {"quote" : "A true artist is not one who is inspired, but one who inspires others."},
-                {"quote" : "You tell me, and I forget. You teach me, and I remember. You involve me, and I learn."},
-                {"quote" : "A great thought begins by seeing something differently, with a shift of the mind's eye."}
-            ],
-            "products": [
-                {"type" : "eBook", "price" : "12.99"},
-                {"type" : "CD-AUDIO", "price" : "30.13"},
-                {"type" : "HARDCOVER", "price" : "24.32"}
-            ]
-        }
+        book : require('data')
     }
 });
 });
@@ -4844,8 +4845,12 @@ module.exports = function (value) {
 
 
 
+
+
+
+
 require.register("navbar/template.html", function(exports, require, module){
-module.exports = '\n\n<header>\n  <section class="content">\n    <div class="book-information">\n      <h1 class="title ac">GOLD</h1>\n      <h2 class="writer ac">By Chris Cleave</h2>\n      <h3 class="publishing-date ac">Published Jun 2012 , 368 pages</h3>\n      <p class="recommadations ac">\n        <a href="#" class="btn font-signpainter">ADD TO MY QUEUE</a>\n        <a href="#" class="btn font-brandon">I\'VE READ IT</a>\n        <a href="#" class="btn font-arial">NO THANKS</a>\n        <a href="#" class="btn">love</a>\n      </p>\n    </div>\n  </section>\n</header>\n<nav>\n  <section class="content ac">\n    <ul id="navigation-items">\n      <li class="item first">\n        <div class="ac">About this</div>\n        <div class="ac">BOOK</div>\n      </li>\n      <li class="item second">\n        <div class="ac">Favourite</div>\n        <div class="ac">QUOTES</div>\n      </li>\n    </ul>\n  </section>\n</nav>';
+module.exports = '<header>\n  <section class="content">\n    <div class="book-information">\n      <h1 class="title ac" v-text="book">GOLD</h1>\n      <h2 class="writer ac">By Chris Cleave</h2>\n      <h3 class="publishing-date ac">Published Jun 2012 , 368 pages</h3>\n      <p class="recommadations ac">\n        <a href="#" class="btn font-signpainter">ADD TO MY QUEUE</a>\n        <a href="#" class="btn font-brandon">I\'VE READ IT</a>\n        <a href="#" class="btn font-arial">NO THANKS</a>\n        <a href="#" class="btn">love</a>\n      </p>\n    </div>\n  </section>\n</header>\n<nav>\n  <section class="content ac">\n    <ul id="navigation-items">\n      <li class="item first">\n        <div class="ac">About this</div>\n        <div class="ac">BOOK</div>\n      </li>\n      <li class="item second">\n        <div class="ac">Favourite</div>\n        <div class="ac">QUOTES</div>\n      </li>\n    </ul>\n  </section>\n</nav>';
 });
 require.register("main/template.html", function(exports, require, module){
 module.exports = '<section id="middle">\n	<div class="content">\n		<section class="book-shelf fl" style="width: 283px;">\n			<img src="public/img/cover.jpg" width="164" />\n		</section>\n		<section class="book-ingredients fl" style="width: 662px;">\n			<div class="title">\n				<div class="">About this</div>\n				<div class="">BOOK</div>\n			</div>\n			<!--\n			<div class="fl">text</div>\n			<div class="book-cart fr" style="width: 300px; height: 215px; padding: 40px; background: #DDD;">book-cart</div>\n			-->\n			<div id="primary" class="text" style="float: left; width: 100%;">\n			   <div id="secondary" class="book-cart fr" style="width: 300px; height: 215px; background: #DDD;">\n			      <p>Put your content here that goes on the right</p>\n			   </div>\n			   <p>Put your content here that goes on the left and should wrap under the right-hand column</p>\n			</div>\n\n		</section>\n	</div>\n	<div class="quotes" style="width: 100%; color: #FFF;">\n		<div class="title ac" style="margin: 0 auto; width: 962px;">\n			<div class="">Favourite</div>\n			<div class="">QUOTES</div>\n		</div>\n		<div class="title ac" style="margin: 0 auto; width: 962px;">\n			<div class="" style="margin-top: 47px;">ADD A QUOTE</div>\n		</div>\n		<div class="title ac" style="margin: 0 auto; width: 962px;">\n			<div style="margin-top: 47px;" class="">\n				<div class="" style="width: 962px; margin: 0 auto;word-break: break-all;position: relative;">\n					<div class="quote-comma-left" style="position: absolute; top: 0; left: 0;">\'\'</div>\n					<div class="quote-text" style="margin: 0 162px 0 154px;/* display: inline; *//* float: left; */">quotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesququotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesquotesotes</div>\n					<div class="quote-comma-right" style="position: absolute;  top: 0; right: 0;">\'\'</div>\n				</div>\n			</div>\n		</div>\n		<div class="title ac" style="margin: 0 auto; width: 962px;">\n			<div class="" style="margin-top: 112px;">VIEW ALL QUOTES</div>\n		</div>\n	</div>\n</section>';
@@ -4882,6 +4887,14 @@ require.alias("yyx990803-vue/src/directives/view.js", "vue-component-example/dep
 require.alias("yyx990803-vue/src/main.js", "vue-component-example/deps/vue/index.js");
 require.alias("yyx990803-vue/src/main.js", "vue/index.js");
 require.alias("yyx990803-vue/src/main.js", "yyx990803-vue/index.js");
+require.alias("config/src/config.js", "vue-component-example/deps/config/src/config.js");
+require.alias("config/src/config.js", "vue-component-example/deps/config/index.js");
+require.alias("config/src/config.js", "config/index.js");
+require.alias("config/src/config.js", "config/index.js");
+require.alias("data/src/book.js", "vue-component-example/deps/data/src/book.js");
+require.alias("data/src/book.js", "vue-component-example/deps/data/index.js");
+require.alias("data/src/book.js", "data/index.js");
+require.alias("data/src/book.js", "data/index.js");
 require.alias("navbar/index.js", "vue-component-example/deps/navbar/index.js");
 require.alias("navbar/index.js", "navbar/index.js");
 
--- app/src/config.js
+++ app/src/config.js
@@ -1,25 +1,25 @@
-Vue.config({
+module.exports = {
 
-	// prefix for directives
+	/* prefix for directives */
 	prefix: 'app',
 
-	// log debug info
+	/* log debug info */
 	debug: false,
 
-	// suppress warnings
+	/* suppress warnings */
 	silent: false,
 
-	// CSS class attached for entering transition
+	/* CSS class attached for entering transitionc*/
 	enterClass: 'v-enter',
 
-	// CSS class attached for leaving transition
+	/* CSS class attached for leaving transitionc*/
 	leaveClass: 'v-leave',
 
-	// Interpolate mustache bindings
+	/* Interpolate mustache bindings */
 	interpolate: true,
 
-	// Interpolation delimiters
-	// default value translates to {{ }} and {{{ }}}
+	/* Interpolation delimiters
+	 default value translates to {{ }} and {{{ }}}*/
 	delimiters: ['{', '}']
 
-});
+};
