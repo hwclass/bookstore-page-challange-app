486eb5593111b46520695b8a530047625abf4ae7
hwclass <harderworking.class@gmail.com>
2014-07-13 21:45:19 +0300

    Added the init file of footer component.

---
+++ app/src/components/views/footer/init.js
@@ -0,0 +1,15 @@
+/*import modules*/
+var Vue = require('vue');
+
+new Vue({
+	el: 'footer',
+	methods : {
+		log : function (msg) {
+			consoole.log(msg)
+		}
+	},
+	computed : {},
+	data: {
+		testo : 'TESTTTTTT'
+	}
+});
